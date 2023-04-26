42f72eaa65af1a561f6dfbc61a8a08aa08debfc7
Shreemayi <shreemayisonti7@gmail.com>
2023-04-26 14:31:47 -0400

    Edited partition file

--- partition_train_val.py
+++ partition_train_val.py
@@ -18,12 +18,14 @@ def main(spark, userID):
     train_small_interactions = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/interactions_train_small.parquet')
     # train_small_tracks = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/tracks_train_small.parquet')
     # train_small_users = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/users_train_small.parquet')
+    print("number of records in train_small_interactions:")
+    train_small_interactions.count()
 
-    print("Printing stats")
-    print("group by user_id count")
-    train_small_interactions.groupBy('user_id').count().show()
-    print("group by user_id and recording_msid count")
-    train_small_interactions.groupBy('user_id', 'recording_msid').count().show()
+    # print("Printing stats")
+    # print("group by user_id count")
+    # train_small_interactions.groupBy('user_id').count().show()
+    # print("group by user_id and recording_msid count")
+    # train_small_interactions.groupBy('user_id', 'recording_msid').count().show()
 
     print('Printing train_small_interactions inferred schema')
     train_small_interactions.printSchema()
@@ -34,10 +36,23 @@ def main(spark, userID):
 
     print("Train set would be")
     train_set = percent_ranked.filter(percent_ranked.percent_rank <= 0.8)
-    train_set.show(150)
+    print("number of records in train_set:")
+    train_set.count()
+    train_set.write.parquet(f'hdfs:/user/ss16270_nyu_edu/train_small.parquet', mode="overwrite", partitionBy='user_id')
+    new_train = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_small.parquet')
+    new_train.show(150)
+    print("number of records in new_train:")
+    new_train.count()
+
     print("Validation set would be")
     val_set = percent_ranked.filter(percent_ranked.percent_rank > 0.8)
-    val_set.show(150)
+    print("number of records in val_set:")
+    val_set.count()
+    val_set.write.parquet(f'hdfs:/user/ss16270_nyu_edu/val_small.parquet', mode="overwrite", partitionBy='user_id')
+    new_val = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_small.parquet')
+    new_val.show(150)
+    print("number of records in new_val:")
+    new_val.count()
 
 
 # Only enter this block if we're in main

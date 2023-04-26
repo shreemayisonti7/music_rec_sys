1b331672c4a3c79060f91e43fd47b82ac53d5527
Shreemayi <shreemayisonti7@gmail.com>
2023-04-26 13:49:10 -0400

    Edited partition file

--- partition_train_val.py
+++ partition_train_val.py
@@ -15,7 +15,6 @@ def main(spark, userID):
     '''
 
     #Perform the same on large files later
-
     train_small_interactions = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/interactions_train_small.parquet')
     # train_small_tracks = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/tracks_train_small.parquet')
     # train_small_users = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/users_train_small.parquet')
@@ -25,20 +24,11 @@ def main(spark, userID):
     train_small_interactions.show(10)
     print(type(train_small_interactions))
     window_partition_by_users = Window.partitionBy('user_id').orderBy('timestamp')
-    train_small_interactions.select('*', percent_rank().over(window_partition_by_users).alias('percent_rank')).show(50)
-
-
-
-    # print('Printing train_small_users inferred schema')
-    # train_small_tracks.printSchema()
-    # train_small_tracks.show(10)
-    #
-    # print('Printing train_small_users inferred schema')
-    # train_small_users.printSchema()
-    # train_small_users.show(10)
-
-
-
+    percent_ranked = train_small_interactions.select('*', percent_rank().over(window_partition_by_users).alias('percent_rank'))
+    print("Train set would be")
+    percent_ranked.filter(percent_ranked.user_id == 53).filter(percent_ranked.percent_rank <= 0.8).show(50)
+    print("Validation set would be")
+    percent_ranked.filter(percent_ranked.user_id == 53).filter(percent_ranked.percent_rank > 0.8).show(50)
 
 
 # Only enter this block if we're in main

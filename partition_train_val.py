650d0de06523b4f638b27f06d23730d9cea0c0a4
unknown <cpjoshi202@gmail.com>
2023-04-26 17:33:32 -0400

    Got avg rating

--- partition_train_val.py
+++ partition_train_val.py
@@ -108,6 +108,9 @@ def main(spark, userID):
 
     query_2 = spark.sql("SELECT recording_msid, COUNT(*) as cum_rating, COUNT(DISTINCT(user_id)) as num_users FROM train_set GROUP BY recording_msid")
     query_2.show()
+    
+    query_3 = spark.sql("SELECT recording_msid, cum_rating/(num_users+10) as avg_rating FROM query_2 ORDER BY AVG_RATING")
+    query_3.show()
 
 # Only enter this block if we're in main
 if __name__ == "__main__":

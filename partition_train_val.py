e0d5b8026ccba80517f8deb9592add86029bae13
Shreemayi <shreemayisonti7@gmail.com>
2023-04-26 14:12:50 -0400

    Edited partition file

--- partition_train_val.py
+++ partition_train_val.py
@@ -19,16 +19,25 @@ def main(spark, userID):
     # train_small_tracks = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/tracks_train_small.parquet')
     # train_small_users = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/users_train_small.parquet')
 
+    print("Printing stats")
+    print("group by user_id count")
+    train_small_interactions.groupBy('user_id').count().show()
+    print("group by user_id and recording_msid count")
+    train_small_interactions.groupBy('user_id', 'recording_msid').count().show()
+
     print('Printing train_small_interactions inferred schema')
     train_small_interactions.printSchema()
     train_small_interactions.show(10)
     print(type(train_small_interactions))
     window_partition_by_users = Window.partitionBy('user_id').orderBy('timestamp')
     percent_ranked = train_small_interactions.select('*', percent_rank().over(window_partition_by_users).alias('percent_rank'))
+
     print("Train set would be")
-    percent_ranked.filter(percent_ranked.user_id == 53).filter(percent_ranked.percent_rank <= 0.8).show(50)
+    train_set = percent_ranked.filter(percent_ranked.percent_rank <= 0.8)
+    train_set.show(150)
     print("Validation set would be")
-    percent_ranked.filter(percent_ranked.user_id == 53).filter(percent_ranked.percent_rank > 0.8).show(50)
+    val_set = percent_ranked.filter(percent_ranked.percent_rank > 0.8)
+    val_set.show(150)
 
 
 # Only enter this block if we're in main

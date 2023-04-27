5b233e48ab9f545e039781fa0a13dea8bb50a3d0
unknown <cpjoshi202@gmail.com>
2023-04-27 12:22:01 -0400

    eval part 2

--- baseline_model.py
+++ baseline_model.py
@@ -43,6 +43,7 @@ def main(spark, userID):
     ######################################################################################################################
     #evaluation
     val_f = val_set.groupby('user_id').agg(F.collect_set('recording_msid')).alias('unique_recordings')
+    val_f.createOrReplaceTempView('val_f')
     ground_truth = val_f.select('unique_recordings').rdd.flatMap(lambda x: x).collect()
     print("Ground truth")
     ground_truth.show()

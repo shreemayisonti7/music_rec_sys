3ccb99fe1414179b812b64fa7278bace2ac6c7ba
unknown <cpjoshi202@gmail.com>
2023-04-26 23:35:13 -0400

    eval part 1

--- baseline_model.py
+++ baseline_model.py
@@ -4,6 +4,7 @@
 import os
 
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 
 
 def main(spark, userID):
@@ -21,20 +22,25 @@ def main(spark, userID):
 
     train_set.createOrReplaceTempView('train_set')
 
-    mu = [0.99, 0.909, 0.5, 0.09, 0.009, 0.0009]
-    beta_i = [0.01, 0.1, 1, 10, 100, 1000]
-    # mu = 1/(1+beta_g)
+#     mu = [0.99, 0.909, 0.5, 0.09, 0.009, 0.0009]
+#     beta_i = [0.01, 0.1, 1, 10, 100, 1000]
+#     # mu = 1/(1+beta_g)
 
-    grouped_result = spark.sql(
-        'SELECT recording_msid, COUNT(user_id) as cum_rating, COUNT(DISTINCT(user_id)) as num_users FROM train_set '
-        'GROUP BY recording_msid ORDER BY cum_rating DESC LIMIT 100')
+#     grouped_result = spark.sql(
+#         'SELECT recording_msid, COUNT(user_id) as cum_rating, COUNT(DISTINCT(user_id)) as num_users FROM train_set '
+#         'GROUP BY recording_msid ORDER BY cum_rating DESC LIMIT 100')
 
-    grouped_result.createOrReplaceTempView("grouped_result")
+#     grouped_result.createOrReplaceTempView("grouped_result")
 
-    baseline_output = spark.sql(
-        f'SELECT recording_msid, (cum_rating - {mu[0]})/(num_users + {beta_i[0]}) as avg_rating FROM grouped_result '
-        f'ORDER BY avg_rating DESC LIMIT 100')
-    baseline_output.show()
+#     baseline_output = spark.sql(
+#         f'SELECT recording_msid, (cum_rating - {mu[0]})/(num_users + {beta_i[0]}) as avg_rating FROM grouped_result '
+#         f'ORDER BY avg_rating DESC LIMIT 100')
+#     baseline_output.show()
+#     baseline_output.createOrReplaceTempView('baseline_output')
+    
+    ######################################################################################################################
+    #evaluation
+    train_set.groupby('user_id').agg(F.collect_set('recording_msid')).show()
 
 
 # Only enter this block if we're in main

c1c775e768b7ad20e306e6e319d98154af05995f
Shreemayi <shreemayisonti7@gmail.com>
2023-04-27 23:13:15 -0400

    Edited baseline for eval file

--- baseline_model.py
+++ baseline_model.py
@@ -10,21 +10,21 @@ from pyspark.mllib.evaluation import RankingMetrics
 import time
 
 
-# def baseline_evaluation(spark, baseline_predictions, test_set):
-#     print("Counting distinct user_id in val set")
-#     test_set.agg(F.countDistinct('user_id')).show()
-#     users = test_set.select('user_id').distinct().rdd.flatMap(lambda x: x).collect()
-#     print(len(users))
-#     map_list = []
-#     for user in users:
-#         current_user_rmsids = test_set.filter(test_set.user_id == user).select('recording_msid').distinct().rdd.flatMap(
-#             lambda x: x).collect()
-#         prediction_Labels = spark.sparkContext.parallelize([(baseline_predictions, current_user_rmsids)])
-#         metrics = RankingMetrics(prediction_Labels)
-#         map_list.append(metrics.meanAveragePrecision)
-#     MAP_final = sum(map_list)/len(users)
-#     print("Final MAP:", MAP_final )
-#     return MAP_final
+def baseline_evaluation(spark, baseline_predictions, test_set):
+    print("Counting distinct user_id in val set")
+    test_set.agg(F.countDistinct('user_id')).show()
+    users = test_set.select('user_id').distinct().rdd.flatMap(lambda x: x).collect()
+    print(len(users))
+    map_list = []
+    for user in users:
+        current_user_rmsids = test_set.filter(test_set.user_id == user).select('recording_msid').distinct().rdd.flatMap(
+            lambda x: x).collect()
+        prediction_Labels = spark.sparkContext.parallelize([(baseline_predictions, current_user_rmsids)])
+        metrics = RankingMetrics(prediction_Labels)
+        map_list.append(metrics.meanAveragePrecision)
+    MAP_final = sum(map_list)/len(users)
+    print("Final MAP:", MAP_final )
+    return MAP_final
 
 
 def main(spark, userID):
@@ -35,7 +35,7 @@ def main(spark, userID):
     '''
     # #################################################################################################################
     # Baseline model
-    # start = time.time()
+    start = time.time()
     train_file = f'hdfs:/user/ss16270_nyu_edu/train_small.parquet'
     val_file = f'hdfs:/user/ss16270_nyu_edu/val_small.parquet'
 
@@ -45,7 +45,6 @@ def main(spark, userID):
 
     val_set = spark.read.parquet(val_file)
     val_set = val_set.repartition("user_id")
-    # val_set.createOrReplaceTempView('val_set')
 
     # beta_g = [0.01, 0.1, 1, 10, 100, 1000, 10000]
     # beta_i = [0.01, 0.1, 1, 10, 100, 1000, 10000]
@@ -54,30 +53,19 @@ def main(spark, userID):
 
     for i in range(len(beta_g)):
         mu = 1/(1+beta_g[i])
-        # grouped_result = spark.sql(
-        #     'SELECT recording_msid, COUNT(user_id) as cum_rating, COUNT(DISTINCT(user_id)) as num_users FROM train_set '
-        #     'GROUP BY recording_msid')
 
         grouped_result = train_set.groupBy('recording_msid').agg(F.count('user_id').alias('listens'), F.countDistinct(
             'user_id').alias('num_users'))
 
-        # grouped_result.createOrReplaceTempView("grouped_result")
-        # baseline_output = spark.sql(
-        #     f'SELECT recording_msid, (cum_rating - {mu[0]})/(num_users + {beta_i[0]}) as avg_rating FROM grouped_result '
-        #     f'ORDER BY avg_rating DESC LIMIT 100')
-
         baseline_output = grouped_result.orderBy((F.col('listens') - mu) / (F.col('num_users') + beta_i[i]),
                                                                             ascending=False).limit(100)
-        # baseline_output = spark.sql(
-        #     f'SELECT recording_msid FROM grouped_result '
-        #     f'ORDER BY (cum_rating - {mu})/(num_users + {beta_i[i]}) DESC LIMIT 100')
         baseline_output.show()
         prediction = baseline_output.select('recording_msid').rdd.flatMap(lambda x: x).collect()
         print("Printing top 100 recording msids")
         print(prediction)
 
-        # map = baseline_evaluation(spark, prediction, val_set)
-        # print(map, beta_g[i], beta_i[i])
+        map = baseline_evaluation(spark, prediction, val_set)
+        print(map, beta_g[i], beta_i[i])
 
     ###################################################################################################################
     # evaluation
@@ -89,8 +77,8 @@ def main(spark, userID):
     # print("Ground truth")
     # print(ground_truth)
 
-    # end = time.time()
-    # print(f"Total time for evaluation:{end - start}")
+    end = time.time()
+    print(f"Total time for evaluation:{end - start}")
     print("The end")
 
 

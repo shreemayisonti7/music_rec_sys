ecb01a38b2e66ceba2bbb6a87767cf277c6f6da3
Shreemayi <shreemayisonti7@gmail.com>
2023-04-28 08:16:27 -0400

    Edited baseline for eval file

--- baseline_model.py
+++ baseline_model.py
@@ -22,22 +22,41 @@ def average_precision_calculator(pred_songs, true_songs):
     return cumulative_average_precision / positives
 
 
+def reciprocal_rank_calculator(pred_songs, true_songs):
+    if len(true_songs) <= 0 or len(pred_songs) <= 0:
+        return 0
+    for i in range(len(pred_songs)):
+        if pred_songs[i] in true_songs:
+            return 1 / (i + 1)
+    return 0
+
+
 def mean_average_precision_eval(baseline_predictions, test_set):
-    udf_metrics = F.udf(lambda ground_truth_songs:
-                        average_precision_calculator(baseline_predictions, ground_truth_songs))
     test_set = test_set.groupBy('user_id').agg(F.collect_list('recording_msid').alias('ground_truth_songs'))
-    test_set = test_set.withColumn('average_precision', udf_metrics(F.col('ground_truth_songs')))
+    udf_ap = F.udf(lambda ground_truth_songs:
+                   average_precision_calculator(baseline_predictions, ground_truth_songs))
+    test_set = test_set.withColumn('average_precision', udf_ap(F.col('ground_truth_songs')))
+    test_set.show()
+
+    udf_rr = F.udf(lambda ground_truth_songs:
+                   reciprocal_rank_calculator(baseline_predictions, ground_truth_songs))
+
+    test_set = test_set.withColumn('reciprocal_rank', udf_rr(F.col('ground_truth_songs')))
+    test_set.show()
+
     mean_average_precision = test_set.agg(F.mean(F.col("average_precision")).alias("mean_average_precision")
                                           ).collect()[0]['mean_average_precision']
-    return mean_average_precision
+    mean_reciprocal_rank = test_set.agg(F.mean(F.col("reciprocal_rank")).alias("mean_reciprocal_rank")
+                                        ).collect()[0]['mean_reciprocal_rank']
+    return mean_average_precision, mean_reciprocal_rank
 
 
 def main(spark):
-    '''
+    """
     Parameters
     spark : SparkSession object
     userID : string, userID to find files in HDFS
-    '''
+    """
     start = time.time()
     train_file = f'hdfs:/user/ss16270_nyu_edu/train_small.parquet'
     val_file = f'hdfs:/user/ss16270_nyu_edu/val_small.parquet'
@@ -58,10 +77,11 @@ def main(spark):
                                                      ascending=False).limit(100)
             prediction = baseline_output.select('recording_msid').rdd.flatMap(lambda x: x).collect()
 
-            current_map = mean_average_precision_eval(prediction, val_set)
-            print(current_map, beta_g[i], beta_i[j])
-            val_maps.append((current_map, beta_g[i], beta_i[j]))
+            current_map, current_mrr = mean_average_precision_eval(prediction, val_set)
+            print(current_map, current_mrr, beta_g[i], beta_i[j])
+            val_maps.append((current_map, current_mrr, beta_g[i], beta_i[j]))
     print(sorted(val_maps, key=lambda x: x[0], reverse=True)[0])
+    print(sorted(val_maps, key=lambda x: x[1], reverse=True)[0])
 
     end = time.time()
     print(f"Total time for evaluation:{end - start}")

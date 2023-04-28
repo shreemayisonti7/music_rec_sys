6131effdc7ef42cb8541d00a7dbfb3604df52b09
Shreemayi <shreemayisonti7@gmail.com>
2023-04-28 07:50:41 -0400

    Edited baseline for eval file

--- baseline_model.py
+++ baseline_model.py
@@ -38,7 +38,7 @@ def mean_average_precision_eval(baseline_predictions, test_set):
     return mean_average_precision
 
 
-def main(spark, userID):
+def main(spark):
     '''
     Parameters
     spark : SparkSession object
@@ -49,20 +49,14 @@ def main(spark, userID):
     start = time.time()
     train_file = f'hdfs:/user/ss16270_nyu_edu/train_small.parquet'
     val_file = f'hdfs:/user/ss16270_nyu_edu/val_small.parquet'
-
     train_set = spark.read.parquet(train_file)
     train_set = train_set.repartition("recording_msid")
-    train_set.createOrReplaceTempView('train_set')
-
     val_set = spark.read.parquet(val_file)
-    val_set = val_set.repartition("user_id")
+    # val_set = val_set.repartition("user_id")
 
-    beta_g = [0.01, 0.1, 1, 10, 100, 1000, 10000]
-    beta_i = [0.01, 0.1, 1, 10, 100, 1000, 10000]
+    beta_g = [10, 100, 1000, 10000]
+    beta_i = [10, 100, 1000, 10000]
     val_maps = []
-    # beta_g = [1000]
-    # beta_i = [1000]
-
     grouped_result = train_set.groupBy('recording_msid').agg(F.count('user_id').alias('listens'), F.countDistinct(
         'user_id').alias('num_users'))
 
@@ -71,10 +65,7 @@ def main(spark, userID):
         for j in range(len(beta_i)):
             baseline_output = grouped_result.orderBy((F.col('listens') - mu) / (F.col('num_users') + beta_i[j]),
                                                      ascending=False).limit(100)
-            # baseline_output.show()
             prediction = baseline_output.select('recording_msid').rdd.flatMap(lambda x: x).collect()
-            # print("Printing top 100 recording msids")
-            # print(prediction)
 
             current_map = mean_average_precision_eval(prediction, val_set)
             print(current_map, beta_g[i], beta_i[j])
@@ -86,16 +77,9 @@ def main(spark, userID):
     print("The end")
 
 
-# Only enter this block if we're in main
+
 if __name__ == "__main__":
     # Create the spark session object
     spark = SparkSession.builder.appName('checkpoint').getOrCreate()
-
-    # Get user userID from the command line to access HDFS folder
-    userID = os.environ['USER']
-    print(userID)
-
-    # Calling main
-    main(spark, userID)
-
+    main(spark)
     spark.stop()

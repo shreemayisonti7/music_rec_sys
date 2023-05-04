127b6d3b3e7de3e6a0adf2cd0de31619a2f7dc34
chaitali <cpjoshi202@gmail.com>
2023-05-04 16:46:05 -0400

    changed to msid to index and joined

--- als.py
+++ als.py
@@ -15,30 +15,74 @@ from pyspark.sql.window import Window
 
 
 def main(spark, userID):
-    print("--------------------------------Converting interactions ids to integer-------------------------------------")
+    print("---------------------------Converting recording_msids to integer for train---------------------------------")
+    # start = time.time()
+    # train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
+    # # val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
+    # #
+    # # test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/test_full_joined.parquet')
+    #
+    # recording_data = train_data.select("recording_msid")
+    # rec_fin = recording_data.groupBy("recording_msid").agg(F.countDistinct("recording_msid").alias("rec_frequency"))
+    #
+    # rec_data = rec_fin.withColumn('recording_index',
+    #                                row_number().over(Window.partitionBy("recording_msid").orderBy("rec_frequency")) - 1)
+    # print("Index data")
+    # rec_data.show()
+    #
+    # train_new = train_data.join(rec_data,on="recording_msid",how="left")
+    # print("Joined data")
+    # train_f = train_new.select("user_id","recording_msid","recording_index")
+    # train_f.show()
+    #
+    # train_f.write.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_als.parquet', mode="overwrite")
+    # end = time.time()
+    # print(f"Time for execution:{end-start}")
+
+    print("---------------------------Converting recording_msids to integer for train---------------------------------")
+    train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_als.parquet')
+    train_data.show()
     start = time.time()
-    train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
-    # val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
+    #train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
+    val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
     #
     # test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/test_full_joined.parquet')
 
-    recording_data = train_data.select("recording_msid")
+    recording_data = val_data.select("recording_msid")
     rec_fin = recording_data.groupBy("recording_msid").agg(F.countDistinct("recording_msid").alias("rec_frequency"))
 
     rec_data = rec_fin.withColumn('recording_index',
-                                   row_number().over(Window.orderBy("rec_frequency")) - 1)
+                                  row_number().over(Window.partitionBy("recording_msid").orderBy("rec_frequency")) - 1)
     print("Index data")
     rec_data.show()
 
-    train_new = train_data.join(rec_data,on="recording_msid",how="left")
+    val_new = val_data.join(rec_data, on="recording_msid", how="left")
     print("Joined data")
-    train_f = train_new.select("user_id","recording_msid","recording_index")
-    train_f.show()
+    val_f = val_new.select("user_id", "recording_msid", "recording_index")
+    val_f.show()
 
-    train_f.write.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_als.parquet', mode="overwrite")
+    val_f.write.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_als.parquet', mode="overwrite")
+
+    print("---------------------------Converting recording_msids to integer for train---------------------------------")
+    test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/test_full_joined.parquet')
+
+    recording_data_t = test_data.select("recording_msid")
+    rec_fin_t = recording_data_t.groupBy("recording_msid").agg(F.countDistinct("recording_msid").alias("rec_frequency"))
+
+    rec_data_t = rec_fin_t.withColumn('recording_index',
+                                  row_number().over(Window.partitionBy("recording_msid").orderBy("rec_frequency")) - 1)
+    print("Index data")
+    rec_data_t.show()
+
+    test_new = test_data.join(rec_data_t, on="recording_msid", how="left")
+    print("Joined data")
+    test_f = test_new.select("user_id", "recording_msid", "recording_index")
+    test_f.show()
+
+    test_f.write.parquet(f'hdfs:/user/ss16270_nyu_edu/test_full_als.parquet', mode="overwrite")
+
     end = time.time()
-    print(f"Time for execution:{end-start}")
-
+    print(f"Time for execution:{end - start}")
 
 
 

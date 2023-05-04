6a5719b41e768b9bae2afdb68dcf64b08b605ca2
chaitali <cpjoshi202@gmail.com>
2023-05-04 12:57:01 -0400

    Converted recroding id to integer

---
+++ als.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+from pyspark.sql import SparkSession
+# from pyspark.sql import Window
+# from pyspark.sql.functions import percent_rank
+#
+# from pyspark.ml.evaluation import RegressionEvaluator
+# from pyspark.ml.recommendation import ALS
+# from pyspark.sql import Row
+from pyspark.ml.feature import StringIndexer
+
+
+def main(spark, userID):
+    print("--------------------------------Converting interactions ids to integer-------------------------------------")
+    start = time.time()
+    train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
+    # val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
+    #
+    # test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/test_full_joined.parquet')
+
+    recording_indexer = StringIndexer(inputCol="recording_msid", outputCol="recordingIndex")
+    # Fits a model to the input dataset with optional parameters.
+    train_new = recording_indexer.fit(train_data).transform(train_data)
+    train_new.show()
+
+    train_new.write.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_als.parquet', mode="overwrite")
+    end = time.time()
+    print(f"Time for execution:{end-start}")
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Create the spark session object
+    spark = SparkSession.builder.appName('checkpoint').getOrCreate()
+
+    # Get user userID from the command line to access HDFS folder
+    userID = os.environ['USER']
+    print(userID)
+
+    # Calling main
+    main(spark, userID)
+
+
+
+
+

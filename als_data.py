54119805ec7dda39493d12d81525d6796845db56
chaitali <cpjoshi202@gmail.com>
2023-05-05 00:53:23 -0400

    Added code to get index for all recrodings

---
+++ als_data.py
@@ -0,0 +1,56 @@
+import os
+import time
+
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import StringIndexer
+import pyspark.sql.functions as F
+from pyspark.sql.functions import desc, row_number, monotonically_increasing_id
+from pyspark.sql.window import Window
+
+
+def main(spark, userID):
+    print("---------------------------Converting recording_msids to integer for train---------------------------------")
+    start=time.time()
+
+    train_tracks = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/tracks_train.parquet')
+    test_tracks = spark.read.parquet(f'hdfs:/user/bm106_nyu_edu/1004-project-2023/tracks_test.parquet')
+
+    new_train = train_tracks.select(when(col('recording_mbid').isNotNull(), col('recording_mbid')
+                                                             ).otherwise(col('recording_msid')).alias('recording_msid')).distinct()
+
+    new_test = test_tracks.select(when(col('recording_mbid').isNotNull(), col('recording_mbid')
+                                                             ).otherwise(col('recording_msid')).alias('recording_msid')).distinct()
+
+    final_df = new_train.union(new_test)
+    final_data = final_df.distinct()
+
+    rec_fin = final_data.groupBy("recording_msid").agg(F.countDistinct("recording_msid").alias("rec_frequency"))
+    rec_data = rec_fin.withColumn('recording_index',
+                                  row_number().over(Window.partitionBy("recording_msid").orderBy("rec_frequency")) - 1)
+    print("Index data")
+    rec_data.show()
+
+    rec_data.write.parquet(f'hdfs:/user/ss16270_nyu_edu/rec_index_als.parquet', mode="overwrite")
+
+    end = time.time()
+    print(f"Time for execution:{end - start}")
+
+
+
+
+
+if __name__ == "__main__":
+    # Create the spark session object
+    spark = SparkSession.builder.appName('checkpoint').getOrCreate()
+
+    # Get user userID from the command line to access HDFS folder
+    userID = os.environ['USER']
+    print(userID)
+
+    # Calling main
+    main(spark, userID)
+
+
+
+
+
--- als_model.py
+++ als_model.py
@@ -14,12 +14,38 @@ def main(spark, userID):
     print("---------------------------Converting recording_msids to integer for train---------------------------------")
     start = time.time()
     train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_als.parquet')
+    val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_als.parquet')
 
     train_data_f = train_data.select("user_id","recording_index")
     grouped_data = train_data_f.groupBy("user_id","recording_index").agg(F.count("recording_index").
                                                                          alias("rec_frequency"))
 
-    grouped_data.show()
+    val_data_f = val_data.select("user_id","recording_index")
+    grouped_data_v = train_data_f.groupBy("user_id", "recording_index").agg(F.count("recording_index").
+                                                                          alias("rec_frequency"))
+
+    als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="recording_index", ratingCol="rec_frequency",
+              coldStartStrategy="drop",implicitPrefs=True)
+    model = als.fit(training)
+
+    # Evaluate the model by computing the RMSE on the test data
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rec_frequency",
+                                    predictionCol="prediction")
+    rmse = evaluator.evaluate(predictions)
+    print("Root-mean-square error = " + str(rmse))
+
+    # Generate top 10 movie recommendations for each user
+    # userRecs = model.recommendForAllUsers(10)
+    # # Generate top 10 user recommendations for each movie
+    # movieRecs = model.recommendForAllItems(10)
+    #
+    # # Generate top 10 movie recommendations for a specified set of users
+    # users = ratings.select(als.getUserCol()).distinct().limit(3)
+    # userSubsetRecs = model.recommendForUserSubset(users, 10)
+    # # Generate top 10 user recommendations for a specified set of movies
+    # movies = ratings.select(als.getItemCol()).distinct().limit(3)
+    # movieSubSetRecs = model.recommendForItemSubset(movies, 10)
 
     end=time.time()
 

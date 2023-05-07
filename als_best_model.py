7b77578f89b7720b2839ee726239b19eb0192b82
chaitali <cpjoshi202@gmail.com>
2023-05-06 22:07:15 -0400

    Added code for hyperparameter tuning

---
+++ als_best_model.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+from pyspark.sql.functions import desc, row_number, monotonically_increasing_id
+from pyspark.sql.window import Window
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.sql import Row
+from pyspark.ml.tuning import ParamGridBuilder,CrossValidator
+from pyspark.ml import Pipeline
+
+def main(spark, userID):
+    print("---------------------------Converting recording_msids to integer for train---------------------------------")
+    best_model = ALSModel.load(f'hdfs:/user/ss16270_nyu_edu/als_model')
+    print(f"Rank: {best_model.Rank()}")
+    print(f"Alpha:{best_model.getRegParam()}")
+    print(f"Reg param:{best_model.getAlpha()}")
+
+
+
+
+
+if __name__ == "__main__":
+    # Create the spark session object
+    spark = SparkSession.builder.appName('checkpoint').getOrCreate()
+
+    # Get user userID from the command line to access HDFS folder
+    userID = os.environ['USER']
+    print(userID)
+
+    # Calling main
+    main(spark, userID)
+
+
+
+
+

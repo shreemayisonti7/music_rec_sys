cdf60889834b84169e2df83b29e3d2c7159d1e6a
Shreemayi <shreemayisonti7@gmail.com>
2023-05-09 12:36:15 -0400

    Modified item id str to int file

--- als_rmsid_str_int.py
+++ als_rmsid_str_int.py
@@ -8,17 +8,17 @@ from pyspark.sql.window import Window
 
 def main(spark):
 
-    print("---------------------------Creating rmsid str-int map and saving as parquet--------------------------------")
+    print("---------------------------Creating rmsid str-int map and saving as parquet------------------------------")
     start = time.time()
 
     train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
 
     unique_msids = train_data.select('recording_msid').distinct()  #<class 'pyspark.sql.dataframe.DataFrame'>
-    unique_msids.count()
+    unique_msids.count().show()
 
     window_order_by_rmsid = Window.orderBy('recording_msid')
     rmsid_mapping = unique_msids.select('recording_msid', rank().over(window_order_by_rmsid).alias('rmsid_int'))
-    rmsid_mapping.count()
+    rmsid_mapping.count().show()
 
     rmsid_mapping.write.parquet(f'hdfs:/user/ss16270_nyu_edu/rmsid_str_int_map.parquet', mode="overwrite")
 
@@ -30,13 +30,15 @@ def main(spark):
 
     rsmid_map = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/rmsid_str_int_map.parquet')
     rsmid_map.count()
+    print("Converting recording_msids to integer for train using the map and generating counts for training ALS")
 
-    # print("Converting recording_msids to integer for train using the map and generating counts for training ALS")
-    #
     # train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/train_full_joined.parquet')
+    # train_set = train_data.join(rsmid_map, on='recording_msid', how='left')
+    # train_set = train_set.select('user_id', 'timestamp')
+    #
     #
     # print("Converting recording_msids to integer for val using the map and generating counts for training ALS")
-    # # val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
+    # val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/val_full_joined.parquet')
 
     end = time.time()
     print(f"Time for execution:{end - start}")

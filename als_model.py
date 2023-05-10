cef7160fac3841b3abd4a8e290e9cec142eb5c60
chaitali <cpjoshi202@gmail.com>
2023-05-09 21:48:15 -0400

    Got recs for val users

--- als_model.py
+++ als_model.py
@@ -2,24 +2,24 @@ import time
 
 from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.recommendation import ALS
+from pyspark.ml.recommendation import ALS, ALSModel
 from pyspark.sql.window import Window
 import pyspark.sql.functions as F
 
 def main(spark):
     start = time.time()
-    train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/als_train_set.parquet')
+    #train_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/als_train_set.parquet')
     val_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/als_val_set.parquet')
-    test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/als_test_set.parquet')
+    #test_data = spark.read.parquet(f'hdfs:/user/ss16270_nyu_edu/als_test_set.parquet')
 
     #This is to handle the case where an item is in val/test but not in train.
     val_data = val_data.dropna()
-    test_data = test_data.dropna()
+    #test_data = test_data.dropna()
 
-    als = ALS(maxIter=5, regParam=0.001, rank=10, alpha=50, userCol="user_id", itemCol="rmsid_int", ratingCol="ratings",
-              coldStartStrategy="drop", implicitPrefs=True)
-    model = als.fit(train_data)
-    model.write().overwrite().save(f'hdfs:/user/ss16270_nyu_edu/als_model')
+    # als = ALS(maxIter=5, regParam=0.001, rank=10, alpha=50, userCol="user_id", itemCol="rmsid_int", ratingCol="ratings",
+    #           coldStartStrategy="drop", implicitPrefs=True)
+    # model = als.fit(train_data)
+    # model.write().overwrite().save(f'hdfs:/user/ss16270_nyu_edu/als_model')
     # Evaluate the model by computing the RMSE on the val data
     # pred_val = model.transform(val_data)
     # print("Printing model transformed validation data")
@@ -36,9 +36,11 @@ def main(spark):
     # print("Root-mean-square test error = " + str(rmse_test))
 
     # Generate top 10 movie recommendations for each user
-    # user_recs = model.recommendForUserSubset(val_data,100)
-    # # user_recs.take(1)
-    # user_recs.write.parquet(f'hdfs:/user/ss16270_nyu_edu/best_recs.parquet', mode="overwrite")
+    model = ALSModel.load(f'hdfs:/user/ss16270_nyu_edu/als_model')
+    user_recs = model.recommendForUserSubset(val_data,100)
+    user_recs.write.parquet(f'hdfs:/user/ss16270_nyu_edu/best_recs.parquet', mode="overwrite")
+    user_recs.take(1)
+
     #
     end = time.time()
 

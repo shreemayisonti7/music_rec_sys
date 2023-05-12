c94c65c8e48dd5df9b9674a04f83b66583966022
chaitali <cpjoshi202@gmail.com>
2023-05-11 23:19:47 -0400

    Got recs for val users

--- als_model.py
+++ als_model.py
@@ -93,8 +93,7 @@ def main(spark):
     print("Making recommendations")
     user_recs = model.recommendForUserSubset(val_data_1,100)
     #
-    # print("Converting to DF")
-    print("Printing user")
+    user_recs = user_recs.withColumn("recommendations", col("recommendations").getField("rmsid_int"))
     print(user_recs.take(1))
     # user_f = user_recs.toDF("user_id","recs")
     # user_recs_1 = user_f.repartition(50, "user_id")

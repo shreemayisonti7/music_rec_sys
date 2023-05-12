f106bd1c265c51fb3d319a1f5505c169bd429d19
chaitali <cpjoshi202@gmail.com>
2023-05-11 23:28:51 -0400

    Got recs for val users

--- als_model.py
+++ als_model.py
@@ -87,7 +87,6 @@ def main(spark):
     user_recs = model.recommendForUserSubset(val_data_1,100)
     #
     user_recs = user_recs.withColumn("recommendations", col("recommendations").getField("rmsid_int"))
-    print(user_recs.take(1))
     # user_f = user_recs.toDF("user_id","recs")
     # user_recs_1 = user_f.repartition(50, "user_id")
     #
@@ -100,7 +99,7 @@ def main(spark):
     # #user_final.repartition(50,"user_id")
     # user_final_1.write.parquet(f'hdfs:/user/ss16270_nyu_edu/val_eval_f.parquet', mode="overwrite")
     #
-    current_map, current_mrr= evaluator(user_final_)
+    current_map, current_mrr= evaluator(user_final)
     print(f"MAP:{current_map}, MRR:{current_mrr}")
 
     end = time.time()
